37562a0b107b51bc7ca9d21ad6d06653b23a043f
Gaspar Dzul <casper088@gmail.com>
2023-03-10 11:33:36 -0600

    COmmit inicial

---
+++ pages/blog/index.js
@@ -0,0 +1,101 @@
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+import {useState} from "react";
+
+const Home =()=> {
+
+    const [pageName,setPageName] = useState("Pagina de capacitación")
+    const numTitles = [1,2,3,4]
+    const objeto = {};
+
+    const PageTitle=({num})=>{
+
+        return (
+            <p>Hola gaspar bienvenido a Blog {pageName} {num}</p>
+        )
+    }
+
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                {
+                    numTitles.map((item)=>{
+                        return <PageTitle key={item} num={item}/>
+                    })
+                }
+
+                <p>{objeto?.persona?.id ? objeto?.persona?.id : 'N/A'}</p>
+
+                <button onClick={()=> setPageName('Pagina de cursos')}>Cambiar valor</button>
+            </main>
+            <footer>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+                </a>
+            </footer>
+
+            <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+            <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+        </div>
+    )
+}
+
+export default Home;
